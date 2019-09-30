1a3a7eaa3f9e59275a650582d15a703a5c0dfdc3
David Otuya <davodeky@yahoo.com>
2019-09-30 13:33:57 -0400

    third

--- comb1.py
+++ comb1.py
@@ -7,8 +7,17 @@ def comb(k, p):
     C = list(comb)
     return C
     
+m = 1
+for i in range(4):
+    C = comb(4,i+1)
+    for j in range(len(C)):
+        print("Sweep #"+str(m)+": ")
+        print("[",end='')
+        for l in range(len(C[j])-1):
+            print("B"+str(C[j][l] + 1)+", ",  end='')
+        print("B"+str(C[j][-1] + 1),  end='')
+        print("]  ----> ")
+        print()
+        m = m + 1
 
-for i in range(5):
-    C = []
-    C = comb(5, i+1)
-    print(C)
+    
